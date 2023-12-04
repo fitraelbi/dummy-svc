d3f350e010a2da70554c8fe68c37ba7a03a470c3
fitraelbi <fitraelbiamrullah@gmail.com>
2023-12-04 13:40:50 +0700

    change version

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World! versi 1")
+        return c.SendString("Hello, World! versi 2")
     })
 
 	app.Get("/salah", func(c *fiber.Ctx) error {
