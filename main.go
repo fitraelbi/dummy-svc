96f3bbf9dc45b86514c9424eb23c133c68c66324
fitraelbi <fitraelbiamrullah@gmail.com>
2023-12-18 10:51:47 +0700

    feat : add endpoint test to check raw message from body request

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
+import "fmt"
+
 func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World! versi 4")
+        return c.SendString("Hello, World! versi 5")
     })
 
 	app.Get("/salah", func(c *fiber.Ctx) error {
@@ -15,6 +17,12 @@ func main() {
     	// 503 On vacation!
    		 return fiber.NewError(fiber.StatusServiceUnavailable, "On vacation!")
     })
+
+    app.Post("/test", func(c *fiber.Ctx) error {
+        // Get raw body from POST request:
+        fmt.Println(string(c.BodyRaw()))
+        return c.Send(c.BodyRaw()) // []byte("user=john")
+      })
 
     app.Listen(":8080")
 }
